1adbefb295980ccfb75130aaed3e8c1619faaf94
이정환 <46296754+winterlood@users.noreply.github.com>
2020-04-02 05:03:59 +0900

    add card view
    expanded card

--- system-monitor/src/component/PcItem/PcItem.js
+++ system-monitor/src/component/PcItem/PcItem.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './PcItem.css';
+import { Row, Collapse, Button, CardBody, Card } from 'reactstrap';
+
 const PcItem = ({id,powerStatus,ramData,cpuData}) => {
+    const Offlight = () =>{
+        return <div className="off-light"></div>
+    }
+    const Onlight =()=>{
+        return <div className="on-light"></div>
+    }
+    const Powerlight = () =>{
+        if(powerStatus==="ON" || powerStatus ==="On"){
+            return <Onlight></Onlight>
+        }
+        else return <Offlight></Offlight>
+    }
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     return(
         <React.Fragment>
+            <Row>
             <div className="pc-item-wrapper">
+            <Powerlight/>
                 <div className="id-box">
-                    <p>{id}</p>
+                    <p>{id}
+                    <Button color="primary" className="expand-button" onClick={toggle}>Show more</Button>
+                    </p>                
                 </div>
-                <p>{powerStatus}</p>
+                <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+            <p>RAM : {ramData}</p> 
+            <p>CPU : {cpuData}</p> 
+          </CardBody>
+        </Card>
+      </Collapse>
+                {/* <p>{powerStatus}</p>
                 <p>{ramData}</p>
-                <p>{cpuData}</p>
+                <p>{cpuData}</p> */}
             </div>
+            </Row>
         </React.Fragment>
     );
 }
