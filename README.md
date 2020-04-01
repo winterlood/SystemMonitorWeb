5e181acdcc78c99b19438c58240aaa2c5774f855
이정환 <46296754+winterlood@users.noreply.github.com>
2020-04-02 04:44:44 +0900

    add bootstrap

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ npm install --save axios
 ~~~
 
 ~~~
+npm install --save bootstrap
+
 npm install --save reactstrap react react-dom
 ~~~
 
--- system-monitor/src/component/TotalPc/TotalPc.js
+++ system-monitor/src/component/TotalPc/TotalPc.js
@@ -1,65 +1,65 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PcItem from '../PcItem/PcItem'
 import One from '../test/One';
-import { Container } from '@material-ui/core';
+import { Container } from 'reactstrap';
 const TotalPc = () => {
-    const [pcs,setPcs] = useState(null);
+    const [pcs, setPcs] = useState(null);
 
     const getPcs = () => {
         axios.get("mobile/pc")
-        .then(function (response) {
-          console.log(response);
-          console.log(response.data.pcs);
+            .then(function (response) {
+                console.log(response);
+                console.log(response.data.pcs);
 
-            setPcs(response.data.pcs.map(({powerStatus,ramData,cpuData,startTime,endTime,id}) => 
-            (
-               <PcItem
-               id={id}
-               powerStatus={powerStatus}
-               ramData={ramData}
-               cpuData={cpuData}
-               />
-            )));
+                setPcs(response.data.pcs.map(({ powerStatus, ramData, cpuData, startTime, endTime, id }) =>
+                    (
+                        <PcItem
+                            id={id}
+                            powerStatus={powerStatus}
+                            ramData={ramData}
+                            cpuData={cpuData}
+                        />
+                    )));
 
-        // test = response.data.pcs.map(({powerStatus,ramData,startTime,endTime,id}) => 
-        // (
-        //     <div>
-        //         <p>{id}</p>
-        //         <p>{ramData}</p>
-        //     </div>
-        // ))
+                // test = response.data.pcs.map(({powerStatus,ramData,startTime,endTime,id}) => 
+                // (
+                //     <div>
+                //         <p>{id}</p>
+                //         <p>{ramData}</p>
+                //     </div>
+                // ))
 
-        console.log("test is");
-        console.log(test);
-            // response.data.pcs.map(({})=>{})
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+                console.log("test is");
+                console.log(test);
+                // response.data.pcs.map(({})=>{})
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getPcs();
         setTimeout(function run() {
             getPcs();
             setTimeout(run, 300000);
-          }, 300000);
-    },[1])
+        }, 300000);
+    }, [1])
 
-    
-  
+
+
     const func = (i) => {
-      console.log(i);
+        console.log(i);
     }
     let i = 1;
-   
-    
-    return(
+
+
+    return (
         <React.Fragment>
             <Container>
-        {pcs}
-        </Container>
+                {pcs}
+            </Container>
         </React.Fragment>
     );
 }
