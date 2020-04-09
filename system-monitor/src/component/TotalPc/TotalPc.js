113fc71072310ab58fd56a081c811e76f1db7583
winterlood <king199777@gmail.com>
2020-04-09 20:00:22 +0900

    on off 분리

--- system-monitor/src/component/TotalPc/TotalPc.js
+++ system-monitor/src/component/TotalPc/TotalPc.js
@@ -11,6 +11,8 @@ const smallPaddingStyle = {
 
 const TotalPc = ({ isPolling, handlePolling }) => {
     const [pcs, setPcs] = useState(null);
+    const [onPcs, setOnPcs] = useState(null);
+    const [offPcs, setOffPcs] = useState(null);
 
     const getPcs = () => {
         axios
@@ -22,8 +24,32 @@ const TotalPc = ({ isPolling, handlePolling }) => {
                 const sortedResponse = response.data.pcs.sort((a, b) => {
                     return a[sortingField] - b[sortingField];
                 });
-                console.log("정렬된 response");
-                console.log(sortedResponse);
+                const filterOn = sortedResponse.filter((it) => it.powerStatus === "ON" || it.powerStatus === "On");
+                setOnPcs(
+                    filterOn.map(({ powerStatus, ramData, cpuData, startTime, endTime, id }) => (
+                        <PcItem
+                            key={id}
+                            id={id}
+                            powerStatus={powerStatus}
+                            ramData={ramData}
+                            cpuData={cpuData}
+                            endTime={endTime}
+                        />
+                    ))
+                );
+                const filterOff = sortedResponse.filter((it) => it.powerStatus === "OFF" || it.powerStatus === "Off");
+                setOffPcs(
+                    filterOff.map(({ powerStatus, ramData, cpuData, startTime, endTime, id }) => (
+                        <PcItem
+                            key={id}
+                            id={id}
+                            powerStatus={powerStatus}
+                            ramData={ramData}
+                            cpuData={cpuData}
+                            endTime={endTime}
+                        />
+                    ))
+                );
                 setPcs(
                     sortedResponse.map(({ powerStatus, ramData, cpuData, startTime, endTime, id }) => (
                         <PcItem
@@ -105,7 +131,10 @@ const TotalPc = ({ isPolling, handlePolling }) => {
                     endTime={"2020-04-04-13-13"}
                 />
             </Container>
-            <Container>{pcs}</Container>
+            <Container>
+                {onPcs}
+                {offPcs}
+            </Container>
         </React.Fragment>
     );
 };
