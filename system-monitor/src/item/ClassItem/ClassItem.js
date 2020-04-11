d6be302b2e11c038193c962bcbf483f61568c3ac
winterlood <king199777@gmail.com>
2020-04-12 03:16:30 +0900

    style fix & routing

--- system-monitor/src/item/ClassItem/ClassItem.js
+++ system-monitor/src/item/ClassItem/ClassItem.js
@@ -1,36 +1,40 @@
 import React from "react";
 import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
 import "./ClassItem.css";
 const ClassItem = () => {
     const classId = "D404";
     const classOnCount = "3";
     const classOffCount = "44";
+    const updateTime = "2020-04-01-13-24-52";
     return (
-        <div className="ClassItem">
-            <div className="class-item-wrapper">
-                <div className="class-id-box-outer">
-                    <div className="class-id-box-inner">{classId}</div>
-                </div>
-                <div className="class-update-log-outer">
-                    <div className="class-update-log-inner">update : 2020-04-01-13-24-52</div>
-                </div>
-
-                <div className="class-power-status-wrapper">
-                    <div className="class-on-box-outer">
-                        <div className="class-on-inner">
-                            <rect className="class-on-circle"></rect>
-                            {classOnCount}
-                        </div>
+        <Link to={"/class/" + classId}>
+            <div className="ClassItem">
+                <div className="class-item-wrapper">
+                    <div className="class-id-box-outer">
+                        <div className="class-id-box-inner">{classId}</div>
                     </div>
-                    <div className="class-off-box-outer">
-                        <div className="class-off-inner">
-                            <rect className="class-off-circle"></rect>
-                            {classOffCount}
+                    <div className="class-update-log-outer">
+                        <div className="class-update-log-inner">{updateTime}</div>
+                    </div>
+
+                    <div className="class-power-status-wrapper">
+                        <div className="class-on-box-outer">
+                            <div className="class-on-inner">
+                                <rect className="class-on-circle"></rect>
+                                {classOnCount}
+                            </div>
+                        </div>
+                        <div className="class-off-box-outer">
+                            <div className="class-off-inner">
+                                <rect className="class-off-circle"></rect>
+                                {classOffCount}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
