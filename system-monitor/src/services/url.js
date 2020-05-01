9592e201519afe9c8fdabb8e5b7f876503a3038b
winterlood <king199777@gmail.com>
2020-05-01 19:41:25 +0900

    Update for firebase hosting

--- system-monitor/src/services/url.js
+++ system-monitor/src/services/url.js
@@ -1,22 +1,22 @@
-const GET_PC_DATA = (pcId) => "http://13.125.208.19/mobile/pc/" + pcId + "/data";
+const GET_PC_DATA = (pcId) => "https://www.22hours.online/mobile/pc/" + pcId + "/data";
 
-const GET_TOTAL_PCS = "http://13.125.208.19/mobile/pc";
+const GET_TOTAL_PCS = "https://www.22hours.online/mobile/pc";
 
-const GET_CLASS = "http://13.125.208.19/mobile/class";
+const GET_CLASS = "https://www.22hours.online/mobile/class";
 
-const GET_CLASS_PCS = (classId) => "http://13.125.208.19/mobile/class/" + classId;
+const GET_CLASS_PCS = (classId) => "https://www.22hours.online/mobile/class/" + classId;
 
-const POST_OFF_ONE_PC = "http://13.125.208.19/mobile/pc/power/";
+const POST_OFF_ONE_PC = "https://www.22hours.online/mobile/pc/power/";
 // powerStatus : "OFF" 추가 할 것
 
-const POST_DELAY_ONE_PC = "http://13.125.208.19/mobile/pc/power/";
+const POST_DELAY_ONE_PC = "https://www.22hours.online/mobile/pc/power/";
 // powerStatus값은 주지 말 것
 
-const POST_OFF_ALL_PC = "http://13.125.208.19/mobile/class/power/";
+const POST_OFF_ALL_PC = "https://www.22hours.online/mobile/class/power/";
 // powerStatus : "OFF" 추가 할 것
 // type :"CLASS"
 
-const POST_DELAY_ALL_PC = "http://13.125.208.19/mobile/class/power/";
+const POST_DELAY_ALL_PC = "https://www.22hours.online/mobile/class/power/";
 // powerStatus : "ON" 추가 할 것
 // type :"CLASS"
 
@@ -24,6 +24,8 @@ const header = {
     "Content-Type": "application/json",
 };
 
+//https://www.22hours.online/
+//http://13.125.208.19/
 export {
     header,
     GET_PC_DATA,
